a0cd001ca0eb5a28f479a275cdfd696530307acb
shashwatksingh <shashwatksingh.27@gmail.com>
2023-01-12 20:25:50 +0530

    adding prisma to the project

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({datasources: {
+        db: {
+            url: 'postgresql://postgres:123@localhost:5435/nest'
+        }
+    }});
+  }
+}

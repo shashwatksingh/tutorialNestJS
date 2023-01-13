8f4f8f957eda9358932d9a36a86dce19553be276
shashwatksingh <shashwatksingh.27@gmail.com>
2023-01-13 10:52:28 +0530

    new migration added with exception handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDTO } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -10,18 +11,26 @@ export class AuthService {
     return { message: 'I am signed in' };
   }
   async signup(dto: AuthDTO) {
-    const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: { email: dto.email, hash },
-      select: {
-        id: true,
-        email: true,
-        first_name: true,
-        last_name: true,
-        created_at: true,
-        updated_at: true,
-      },
-    });
-    return user;
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash },
+        select: {
+          id: true,
+          email: true,
+          first_name: true,
+          last_name: true,
+          created_at: true,
+          updated_at: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if ((error.code = 'P2002')) {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+    }
   }
 }

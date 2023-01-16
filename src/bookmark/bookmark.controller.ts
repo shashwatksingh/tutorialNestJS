6240fb3f4c706cf8cb78b8010694e71899db21b0
shashwatksingh <shashwatksingh.27@gmail.com>
2023-01-16 09:32:00 +0530

    adding the crud for bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,27 +1,28 @@
-import { Controller, Delete, Get, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 
 @Controller('bookmarks')
 @UseGuards(JwtGuard)
 export class BookmarkController {
-    @Get()
-    getBookmarks() {
+  @Get()
+  getBookmarks() {}
 
-    }
+  @Post()
+  createBookmark() {}
 
-    @Get()
-    getBookmarkById() {
+  @Get()
+  getBookmarkById() {}
 
-    }
+  @Patch()
+  editBookmarkById() {}
 
-    @Patch()
-    editBookmarkById() {
-
-    }
-
-    @Delete()
-    deleteBookmarkById() {
-
-    }
-
+  @Delete()
+  deleteBookmarkById() {}
 }
